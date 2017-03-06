4b6ac10c236920e0d7e4ddf3df4cc3dcd08e7c3d
Jeff Sutherland <sutherland.jeffrey.j@gmail.com>
2017-03-05 21:29:49 -0500

    created experiment handler for creating / editing experiment

--- tp/forms.py
+++ tp/forms.py
@@ -1,7 +1,20 @@
 from django import forms
+from django.forms import ModelForm
+from .models import Experiment
+import logging
+import pprint
 
-class experimentForm(forms.Form):
-    experiment_name = forms.CharField(label='Experiment name', max_length=100)
+logger = logging.getLogger(__name__)
+
+class experimentForm(ModelForm):
+    class Meta:
+        model = Experiment
+        #fields = [f.name for f in model._meta.get_fields()]
+        #logger.error(pprint.pformat(fields))
+        #fields = '__all__'
+        # TODO - neither of __all__ or exclude recommended due to security risks per doc; list explicitly once model is stable
+        exclude = ['date_created', 'owner']
+
 
 class uploadDataForm(forms.Form):
     experiment_name = forms.CharField(label='Experiment name', max_length=100)
--- tp/views.py
+++ tp/views.py
@@ -8,6 +8,11 @@ from django.http import HttpResponseRedirect
 from .models import Experiment
 from .forms import experimentForm, uploadDataForm
 
+import logging
+import pprint
+
+logger = logging.getLogger(__name__)
+
 
 def index(request):
     """ the home page for tp """
@@ -23,22 +28,34 @@ def experiments(request):
 def experiment(request, experiment_id=None):
     """ edit/view the definition of an experiment"""
 
+    logger.error("Received experiment_id %s", experiment_id)
+
     # if this is a POST request we need to process the form data
     if request.method == 'POST':
-        # create a form instance and populate it with data from the request:
-        form = experimentForm(request.POST)
+        if experiment_id is not None:
+            exp = Experiment.objects.get(id=experiment_id)
+            form = experimentForm(request.POST, instance=exp)
+        else:
+            form = experimentForm(request.POST)
+
         # check whether it's valid:
         if form.is_valid():
-            # process the data in form.cleaned_data as required
-            # ...
-            # redirect to a new URL:
-            return HttpResponseRedirect('/thanks/')
+            new_exp = form.save()
+            logger.info("Created/edited experiment id %s", new_exp.id)
+            return HttpResponseRedirect('/experiments/')
 
-    # if a GET (or any other method) we'll create a blank form
+    # present blank or completed form when editing existing experiment
     else:
-        form = experimentForm()
+        # retrieve form with existing data for given experiment id (validate first)
+        if experiment_id is not None:
+            exp = Experiment.objects.get(id=experiment_id)
+            form = experimentForm(instance=exp)
+        # return a blank form for experiment creation
+        else:
+            form = experimentForm()
 
-    return render(request, 'experiment.html', {'form': form})
+    context = {'form': form}
+    return render(request, 'experiment.html', context)
 
 def upload_data(request):
     """ upload the data for the experiment after experiment meta data is captured"""
