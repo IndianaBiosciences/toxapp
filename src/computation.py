1da0af6e0fc793469f48157e38da773390cc0ba7
Jeff Sutherland <sutherland.jeffrey.j@gmail.com>
2017-05-03 11:51:49 -0400

    reverting to Dan code in calc group fc

--- src/computation.py
+++ src/computation.py
@@ -73,23 +73,22 @@ class Computation:
     def calc_fold_change(self, config):
         """ calculate group fold change from files in tmpdir and meta data received from webapp in config json file """
 
-        # TODO - this is where Dan/Meeta plug in group fold change calc
         logger.info('Starting fold change calculation in directory %s using config %s', self.tmpdir, config)
-        file = self.tmpdir + '/groupFC.txt'
+        #file = self.tmpdir + '/groupFC.txt'
 
-        # computation_config = settings.COMPUTATION
-        # script_dir = computation_config["script_dir"]
-        # script = os.path.join(script_dir, "computeGFC.py")
-        # outfile = "groupFC.txt"
-        # logger.info("calc_fold_change: config is %s", pprint.pformat(config))
-        # script_cmd = "cd " + config['tmpdir'] + '; python ' + script + " -i " + config['expfile'] + " -o "
-        # script_cmd = script_cmd + outfile + " -s " + script_dir + " -l " + config['logfile']
-        # file = os.path.join(config['tmpdir'], outfile)
-        # logger.info("calc_fold_change: command %s ", script_cmd)
-        # #output = subprocess.getoutput(script_cmd)
+        computation_config = settings.COMPUTATION
+        script_dir = computation_config["script_dir"]
+        script = os.path.join(script_dir, "computeGFC.py")
+        outfile = "groupFC.txt"
+        logger.info("calc_fold_change: config is %s", pprint.pformat(config))
+        script_cmd = "cd " + config['tmpdir'] + '; python ' + script + " -i " + config['expfile'] + " -o "
+        script_cmd = script_cmd + outfile + " -s " + script_dir + " -l " + config['logfile']
+        file = os.path.join(config['tmpdir'], outfile)
+        logger.info("calc_fold_change: command %s ", script_cmd)
+        output = subprocess.getoutput(script_cmd)
         # TODO remove once meeta script running
-        src = settings.BASE_DIR + '/data/sample_fc_data_DM_gemfibrozil_1d_7d_100mg_700_mg.txt'
-        shutil.copyfile(src, file)
+        #src = settings.BASE_DIR + '/data/sample_fc_data_DM_gemfibrozil_1d_7d_100mg_700_mg.txt'
+        #shutil.copyfile(src, file)
 
         logger.info('calc_fold_change: Done fold change calculation; results in %s', file)
         return file
@@ -237,7 +236,7 @@ class Computation:
             row_as_bytes = str.encode(row)
             gmt.write(row_as_bytes)
 
-            if sig_count > 100000 and logger.isEnabledFor(logging.DEBUG):
+            if sig_count > 100 and logger.isEnabledFor(logging.DEBUG):
                 logger.warning('Limited to 100 gene sets in DEBUG mode')
                 break
 
