ad58c5caf4eac797251f883b677af25bd6ca98b5
Sutherland <sutherland@thinksano.com>
2017-05-29 21:15:13 -0400

    add creation of temp celery dir

--- setup_server.sh
+++ setup_server.sh
@@ -5,3 +5,7 @@ sudo apt-get install python-psycopg2
 sudo apt-get install libpq-dev
 sudo apt-get install python3-dev
 sudo apt-get install pgloader
+
+# location where celery will store communication files
+sudo mkdir -p /tmp/celery/results
+sudo chmod 777 /tmp/celery/results
