c0ce1dc8227df56831be089eeaaa29cfeb954353
Jeffrey Sutherland <sutherland.jeffrey.j@gmail.com>
2017-06-01 09:47:10 -0400

    updated celery test scripts to use toxapp framework

--- tests/test_celery.py
+++ tests/test_celery.py
@@ -1,19 +1,13 @@
-from celery import Celery
+import os
 import time
+from django.core.wsgi import get_wsgi_application
 
-# run the worker as celery -A test_celery worker --loglevel=info
-# as root run
-# sudo mkdir -p /tmp/celery/results
-# sudo chmod 777 /tmp/celery/results
-app = Celery('test_celery', backend='file:///tmp/celery/results', broker='amqp://guest:guest@localhost:5672//')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "toxapp.settings")
+application = get_wsgi_application()
 
-@app.task
-def add(x, y):
-    return x + y
+from tp.tasks import add
 
-if True:
-
-    res = add.delay(5,2)
-    time.sleep(5)
-    print(res.ready())
-    print(res.get(timeout=1))
+res = add.delay(5,2)
+time.sleep(5)
+print(res.ready())
+print(res.get(timeout=1))
---
+++ tests/test_celery_standalone.py
@@ -0,0 +1,19 @@
+from celery import Celery
+import time
+
+# run the worker as celery -A test_celery worker --loglevel=info
+# as root run
+# sudo mkdir -p /tmp/celery/results
+# sudo chmod 777 /tmp/celery/results
+app = Celery('test_celery', backend='file:///tmp/celery/results', broker='amqp://guest:guest@localhost:5672//')
+
+@app.task
+def add(x, y):
+    return x + y
+
+if True:
+
+    res = add.delay(5,2)
+    time.sleep(5)
+    print(res.ready())
+    print(res.get(timeout=1))
