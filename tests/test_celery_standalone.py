16f2f37c0b362ddf72ffc6f635dc58256a220835
Sutherland <sutherland@thinksano.com>
2017-08-08 09:56:34 -0400

    changed naming for celery standalone test

--- tests/test_celery_standalone.py
+++ tests/test_celery_standalone.py
@@ -1,11 +1,11 @@
 from celery import Celery
 import time
 
-# run the worker as celery -A test_celery worker --loglevel=info
+# run the worker as celery -A test_celery_standalone worker --loglevel=info
 # as root run
 # sudo mkdir -p /tmp/celery/results
 # sudo chmod 777 /tmp/celery/results
-app = Celery('test_celery', backend='file:///tmp/celery/results', broker='amqp://guest:guest@localhost:5672//')
+app = Celery('test_celery_standalone', backend='file:///tmp/celery/results', broker='amqp://guest:guest@localhost:5672//')
 
 @app.task
 def add(x, y):
